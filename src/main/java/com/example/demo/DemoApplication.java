d8fbdf7970347c31c8770c3d4fb1797cec454fa6
Utkarsh Chakrwarti <utkarsh@tatsam.in>
2022-04-01 15:33:35 +0530

    Simple Rest Api

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,13 @@
-package com.utkarsh.OAuth.With.Google;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OAuthWithGoogleApplication {
+public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(OAuthWithGoogleApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,10 +1,10 @@
-package com.utkarsh.OAuth.With.Google;
+package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class OAuthWithGoogleApplicationTests {
+class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
